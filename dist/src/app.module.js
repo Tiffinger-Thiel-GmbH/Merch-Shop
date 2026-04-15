3a5d97e963b7257884eebab2f933d8b0c71fb86b
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-04-15 17:53:17 +0200

    add standart eslint config and swagger ui and fix bugs and connection issues

--- dist/src/app.module.js
+++ dist/src/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const prisma_module_1 = require("./prisma/prisma.module");
@@ -16,7 +17,7 @@ let AppModule = class AppModule {
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [prisma_module_1.PrismaModule],
+        imports: [config_1.ConfigModule.forRoot(), prisma_module_1.PrismaModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
