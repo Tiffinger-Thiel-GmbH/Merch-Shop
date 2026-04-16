3b0847df3e5d0b0c448ef118a74e4606a00c6fbe
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-04-16 10:37:50 +0200

    fix init

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('API Title')
-    .setDescription('API Beschreibung')
-    .setVersion('1.0')
-    .build();
+
+  const config = new DocumentBuilder().setTitle('API Title').setDescription('API Beschreibung').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
