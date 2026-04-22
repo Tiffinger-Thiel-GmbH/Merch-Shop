4549165b0f71b5dc96617c7c3d14f62c0e37248d
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-04-22 09:27:15 +0200

    put import on top

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -4,8 +4,8 @@ import { ProductsController } from './products.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [PrismaModule],
 })
 export class ProductsModule {}
