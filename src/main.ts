8ffc093c2573824ee396d51d48b63c4787144b6d
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-04-22 09:26:17 +0200

    add port listener

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder().setTitle('API Title').setDescription('API Beschreibung').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
   app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
+  const url = await app.getUrl();
+  const swaggerUrl = url.endsWith('/') ? `${url}api` : `${url}/api`;
+  console.log(`Swagger UI is running on port: ${swaggerUrl}`);
 }
 
 void bootstrap();
