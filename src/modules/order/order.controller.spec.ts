1f77121b0e3b1710c96474bbe8bf6dc2affaeee3
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-07-08 14:30:34 +0200

    update ci-cd and write controller test

--- src/modules/order/order.controller.spec.ts
+++ src/modules/order/order.controller.spec.ts
@@ -4,17 +4,34 @@ import { OrderService } from './order.service';
 
 describe('OrderController', () => {
   let controller: OrderController;
+  let orderService: OrderService;
+
+  const mockOrderService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
-      providers: [OrderService],
+      providers: [
+        {
+          provide: OrderService,
+          useValue: mockOrderService,
+        },
+      ],
     }).compile();
 
     controller = module.get<OrderController>(OrderController);
+    orderService = module.get<OrderService>(OrderService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should inject the mocked order service', () => {
+    expect(orderService).toBeDefined();
+    expect(orderService).toBe(mockOrderService);
   });
 });
