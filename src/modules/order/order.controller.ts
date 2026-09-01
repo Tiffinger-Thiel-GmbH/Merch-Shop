bedb7c7d4ee3aaf5baf38c7cfd902c2b6dafb12e
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-09-01 15:46:33 +0200

    fix errors due to wrong merching

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,9 +1,8 @@
-import { Controller, Post, Body, Param, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDTO } from './dto/create-order/create-order.dto';
 import { OrderDTO } from './dto/order/order.dto';
 import { toOrderDTO } from './dto/to-order/to-order-dto.mapper';
-import { UpdateOrderStatusDTO } from './dto/update-order-status/update-order-status.dto';
 import { MailService } from '../mail/mail.service';
 
 @Controller('orders')
@@ -19,11 +18,5 @@ export class OrderController {
     const orderDTO = toOrderDTO(newOrder);
     const newMail = await this.mailService.sendOrderActionEmail(orderDTO);
     return orderDTO;
-  }
-
-  @Get(':id/status')
-  async updateStatus(@Param('id') id: string, @Query() updateOrderStatusDTO: UpdateOrderStatusDTO): Promise<OrderDTO> {
-    const updatedOrder = await this.orderService.updateStatus(id, updateOrderStatusDTO.action);
-    return toOrderDTO(updatedOrder);
   }
 }
