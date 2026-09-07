9c50e2584a04e44f3d6fd344cadea7f261f75e0c
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-09-07 11:15:06 +0200

    remove update-order-status-dto

--- src/modules/order/dto/update-order-status/update-order-status.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { OrderStatus } from '../../../../generated/prisma/client';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateOrderStatusDTO {
-  @ApiProperty({ enum: OrderStatus })
-  @IsEnum(OrderStatus)
-  status!: OrderStatus;
-}
