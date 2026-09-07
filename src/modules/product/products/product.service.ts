17ac3df90c53b25d1bdfc318bdb4a7d28be1c2a4
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-09-07 14:14:44 +0200

    add product images back

--- src/modules/product/products/product.service.ts
+++ src/modules/product/products/product.service.ts
@@ -9,4 +9,22 @@ export class ProductService {
   public async findAll(): Promise<Product[]> {
     return await this.prisma.product.findMany();
   }
+  public async findOneById(id: string): Promise<Product> {
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        createdAt: true,
+        imageUrl: true,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
+  }
 }
