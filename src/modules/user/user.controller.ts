6a0bbca62dc5789b687a0063f075db7f0dd03de5
Connor McSwain <c.mcswain@tiffinger-thiel.de>
2026-09-17 17:19:53 +0200

    fix: status code für user route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, HttpCode, HttpStatus, Put } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Put, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpsertUserDto } from './dto/upsert-user.dto';
-import { User } from '../../generated/prisma/client';
+import { ApiResponse } from '@nestjs/swagger';
+import { UserDTO } from './dto/user.dto';
+import { toUserDTO } from './dto/to-user-dto.mapper';
+import type { Response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Put()
-  @HttpCode(HttpStatus.CREATED)
-  putUser(@Body() dto: UpsertUserDto): Promise<User> {
-    return this.userService.putUser(dto);
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'User wurde neu angelegt', type: UserDTO })
+  @ApiResponse({ status: HttpStatus.OK, description: 'User ist vorhanden', type: UserDTO })
+  async putUser(@Body() dto: UpsertUserDto, @Res({ passthrough: true }) res: Response): Promise<UserDTO> {
+    const user = await this.userService.putUser(dto);
+    res.status(user.created ? HttpStatus.CREATED : HttpStatus.OK);
+    return toUserDTO(user.user);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { UpsertUserDto } from './dto/upsert-user.dto';
-import { User } from '../../generated/prisma/client';
+import { UserResponseDTO } from './dto/put-user-response.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async putUser(dto: UpsertUserDto): Promise<User> {
+  async putUser(dto: UpsertUserDto): Promise<UserResponseDTO> {
     const existing = await this.prisma.user.findUnique({
       where: { email: dto.userMail },
     });
 
     if (existing) {
-      return existing;
+      return { created: false, user: existing };
     }
 
-    return this.prisma.user.create({
+    const newUser = await this.prisma.user.create({
       data: {
         email: dto.userMail,
         name: dto.userName,
       },
     });
+
+    return { created: true, user: newUser };
   }
 }
